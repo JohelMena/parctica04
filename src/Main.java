4eda05fdd76af6ed8eadff800e0b4fcee1a42bfe
JohelMena <jomenabe@est.utn.ac.cr>
2024-06-03 18:34:23 -0600

    inicio practica4

---
+++ src/Main.java
@@ -0,0 +1,62 @@
+/*=================================================================================
+Study Center....: Universidad TÃ©️cnica Nacional
+Campus..........: PacÃ­fico (JRMP)
+College career..: IngenierÃ­a en TecnologÃ­as de InformaciÃ³n
+Period..........: 2C-2024
+Course..........: ITI-221 - ProgramaciÃ³n I
+Document........: class_01 - main.java
+Goals...........: practice for the exam
+Professor.......: Jorge Ruiz (york)
+Student.........:
+=================================================================================*/
+
+
+
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+
+       /* System.out.println("digite un numero para determinar el valor de n ");
+
+        int  n= scanner.nextInt();
+        int sumatoria=0;
+
+        for (int i = 1; i <= n; i++) {
+
+            System.out.println("digite el valor de A");
+            int A = scanner.nextInt();
+            System.out.println("digite el valor de B");
+            int B = scanner.nextInt();
+            System.out.println("digite el valor de C");
+            int C = scanner.nextInt();
+
+            sumatoria= sumatoria+(A-(B+C));
+
+            System.out.println(sumatoria);
+
+
+        }*/
+
+       /* System.out.println("digite el valor de a");
+        int a= scanner.nextInt();
+        System.out.println("digite el valor de b");
+        int b= scanner.nextInt();
+        System.out.println("digite el valor de c");
+        int c= scanner.nextInt();
+
+        int x= (b * b + (c * 3 % (a - 2 % c)));
+
+        System.out.println("el resultado es "+x);*/
+
+
+
+
+
+
+    }
+}
